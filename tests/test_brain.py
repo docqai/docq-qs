182fe385a46d895058ca69dc5b3c45c17289050c
Chen Wang <dev@chenwang.org>
2023-05-10 21:39:31 +0100

    Refactor how env vars and configs are loaded

--- tests/test_brain.py
+++ tests/test_brain.py
@@ -1,18 +1,19 @@
 import pytest
 from source import answer_brain_chain, answer_brain_agent, init_envs
 
-init_envs()
 
 SAMPLE_QUESTION = "Give me examples of new tools appearing in technology radar?"
 
 @pytest.mark.integrations
 def test_answer_via_chain():
+    init_envs()
     text = answer_brain_chain(SAMPLE_QUESTION)
     assert text is not None
     print(text)
 
 @pytest.mark.integrations
 def test_answer_via_agent():
+    init_envs()
     text = answer_brain_agent(SAMPLE_QUESTION)
     assert text is not None
     print(text)
